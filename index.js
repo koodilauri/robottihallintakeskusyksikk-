f9c667346b07a2ce32ee4ce4f2b01403888cdb4e
koodilauri <lakoivisto@gmail.com>
2015-09-25 23:33:03 +0300

    jotain 2

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ app.get('/', function (req, res) {
 
 app.use(express.static('public'));
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+app.set('port', (process.env.PORT || 3000));
 
-    console.log('Example app listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function () {
+    console.log('app is listening on port', app.get('port'));
 });
